f4b8cd4073a0d737782c491bb87ae02667dedda2
BenAgada <BenAgada>
2023-06-19 12:24:51 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there 
- * Description - print random number */
+/* betty style doc for function main goes there */
 int main(void)
-
 {
 	int n, lastd;
 
