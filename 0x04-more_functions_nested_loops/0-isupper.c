88dd1a96848678cd10dadcf7c55e0380fb9aaa34
BenAgada <BenAgada>
2023-06-22 07:47:19 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  _isupper - Check if a letter is upper
+ *  @c: The number to be checked
+ *  Return: 1 for uppercase character or 0 for anythind else
+ */
+
+int _isupper(int c)
+
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
